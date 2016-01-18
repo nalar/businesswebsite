e7980670b0bab1dfaae3e61b0b0db37d1458a443
Thijs Tel <thijstel@gmail.com>
2016-01-18 20:48:32 +0100

    Extra modifications to the slider

--- js/nycdbFunctions.js
+++ js/nycdbFunctions.js
@@ -1,4 +1,5 @@
 var activeSlide = 0;
+var activeSlideDiv = 1;
 
 $(document).ready(function() {
 
@@ -21,27 +22,55 @@ $(document).ready(function() {
 
 });
 
+// function mainSlide(){
+//     console.log("Mainslide called");
+//     if(activeSlide === sliderImages.length-1){
+//         console.log("Last slide so reset to first");
+//         activeSlide = 0; 
+//         $("#mainSlider").animate({opacity: 0.25}, 1000, function() {
+//             $("#mainSlider").css("background-image","url("+sliderImages[activeSlide]+")");
+//             return($("#mainSlider").animate({opacity: 1.0}, 1000, function() {}));
+//         });
+
+//     } else{
+//         console.log("time for the next slide");
+//         activeSlide++
+//         $("#mainSlider").animate({opacity: 0.25}, 1000, function() {
+//             $("#mainSlider").css("background-image","url("+sliderImages[activeSlide]+")");
+//             return($("#mainSlider").animate({opacity: 1.0}, 1000, function() {}));
+//         });
+
+//     }
+// }
+
 function mainSlide(){
     console.log("Mainslide called");
-    if(activeSlide === sliderImages.length-1){
-        console.log("Last slide so reset to first");
-        activeSlide = 0; 
-        $("#mainSlider").animate({opacity: 0.25}, 1000, function() {
-            $("#mainSlider").css("background-image","url("+sliderImages[activeSlide]+")");
-            return($("#mainSlider").animate({opacity: 1.0}, 1000, function() {}));
-        });
-
+    if(activeSlideDiv = 1){
+        if(activeSlide === sliderImages.length-1){
+            activeSlide = 0;
+            $("#mainSlider2").css("background-image","url("+sliderImages[activeSlide]+")");
+            $("#mainSlider").animate({opacity: 0.0}, 1000, start($("#mainSlider2").animate({opacity: 1.0}, 2000, function(){})));
+            return(activeSlideDiv = 2);
+        } else{
+            activeSlide++
+            $("#mainSlider2").css("background-image","url("+sliderImages[activeSlide]+")");
+            $("#mainSlider").animate({opacity: 0.0}, 1000, start($("#mainSlider2").animate({opacity: 1.0}, 2000, function(){})));
+            return(activeSlideDiv = 2);
+        }
     } else{
-        console.log("time for the next slide");
-        activeSlide++
-        $("#mainSlider").animate({opacity: 0.25}, 1000, function() {
+        if(activeSlide === sliderImages.length-1){
+            activeSlide = 0; 
             $("#mainSlider").css("background-image","url("+sliderImages[activeSlide]+")");
-            return($("#mainSlider").animate({opacity: 1.0}, 1000, function() {}));
-        });
-
+            $("#mainSlider2").animate({opacity: 0.0}, 1000, start($("#mainSlider").animate({opacity: 1.0}, 2000, function(){})));
+            return(activeSlideDiv = 1);
+        } else{
+            activeSlide++
+            $("#mainSlider").css("background-image","url("+sliderImages[activeSlide]+")");
+            $("#mainSlider2").animate({opacity: 0.0}, 1000, start($("#mainSlider").animate({opacity: 1.0}, 2000, function(){})));
+            return(activeSlideDiv = 1);
+        }
     }
 }
-
 
 function showDetails(){
     // This function will open an information window showing
