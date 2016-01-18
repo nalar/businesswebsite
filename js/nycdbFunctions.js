3c9dc69a37d9c5d1d6a0be72ed70aad1777dec8a
Nalar <thijstel@gmail.com>
2016-01-18 11:46:36 +0100

    Added mockups

---
+++ js/nycdbFunctions.js
@@ -0,0 +1,15 @@
+function fadeHome(){
+	// This function will slowly fade the images on the 
+	// landing page in to each other
+};
+
+function showDetails(){
+	// This function will open an information window showing
+	// the details of the clicked object (product or person)
+}
+
+function sendMail(){
+	// Pretend we sent a mail with the details as entered in
+	// the form on the contact page. Just open a window with
+	// confirmation about it.
+}
